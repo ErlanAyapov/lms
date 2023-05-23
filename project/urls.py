2427fa2db8aee9c228abd147cfcf95b226b83f0c
ErlanAyapov <simsiti0009ggg@gmail.com>
2023-05-23 22:09:25 +0600

    changed

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-<<<<<<< HEAD
+# <<<<<<< HEAD
     path('api/feedback/', save_feedback),
-]
-=======
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> origin
+# >>>>>>> origin
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
