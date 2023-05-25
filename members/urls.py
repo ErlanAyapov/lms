29d9f403c8b4292504741afe8e26221867fb5f0d
ErlanAyapov <simsiti0009ggg@gmail.com>
2023-05-25 22:22:06 +0600

    edited functions

--- members/urls.py
+++ members/urls.py
@@ -6,13 +6,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('api/register/', register_view_api_view, name='register'),
-    path('api/auth/', login_view_api_view, name='auth'),
-    path('api/logout/', logout_view_api_view, name='logout'),
-    path('api/register-finish/', user_customer_api_view, name = 'register-finish'),
-    path('api/user/<int:pk>/', user_detail, name='profile'),
-    path('api/user-customer/<int:pk>/', user_customer_detail, name='profile-detail'),
-    path('api/user-customer-create/', user_customer_create, name='user-customer-create'),
+    path('api/register/', register_view_api_view, name='register'), 
+    path('api/user/<int:pk>/', UserDetail.as_view(), name='profile'),
+    path('api/user-customer/<int:pk>/', UserCustomerDetail.as_view(), name='user-customer'),
+    path('api/user-customer-create/', UserCustomerCreate.as_view(), name='user-customer-create'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- members/views.py
+++ members/views.py
@@ -6,6 +6,9 @@ from .serializers import *
 from rest_framework.request import Request
 from rest_framework.response import Response  
 from django.contrib.auth.hashers import make_password
+from rest_framework import generics
+
+
 
 User = get_user_model()
 
@@ -23,82 +26,25 @@ def register_view_api_view(request):
 	return Response({"message": "Данные не корректные!"}, status=400)
 
 
-@api_view(['POST'])
-def login_view_api_view(request):
-	serializer = LoginSerializer(data=request.data)  
+class UserCreate(generics.GenericAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserRegisterSerializer
 
-	if serializer.is_valid():
-		username = serializer.validated_data['username']
-		password = serializer.validated_data['password'] 
-		user = authenticate(request, username = username, password = password) 
-		
-		if user is not None:
-			login(request, user)
-			return Response(serializer.data, status = 201) 
-	return Response({"message":"Данные не корректный!"}, status = 400)
+	def post(self, request, *args, **kwargs):
+		# print(self)
+		return self.create(request, *args, **kwargs)
 
 
-@api_view(['POST'])
-def logout_view_api_view(request):
-	logout(request)
-	return Response({'success': 'Выход из портала сделано!'})
+class UserCustomerCreate(generics.CreateAPIView):
+	queryset = UserCustomer.objects.all()
+	serializer_class = UserCustomerSerializer
 
 
-@api_view(['POST'])
-def user_customer_api_view(request):
-	serializer = UserCustomerSerializer(data = request.data)
-
-	if serializer.is_valid():
-		serializer.save()
-		return Response({'success': 'Регистрация завершена!'}, status = 201)
-	else:
-		return Response({"message":"Данные не корректный!"}, status = 400)
-
-@api_view(['POST'])
-def user_customer_create(request):  
-	if request.method == 'POST':
-		serializer = UserCustomerSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response({"message":"Данные не корректный!"}, status=400)
+class UserCustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = UserCustomer.objects.all()
+	serializer_class = UserCustomerSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def user_customer_detail(request, pk): 
-	try:
-		user_customer = UserCustomer.objects.get(pk=pk)
-		if request.method == 'GET':
-			serializer = UserCustomerSerializer(user_customer)
-			return Response(serializer.data)
-
-		elif request.method == 'PUT': 
-			serializer = UserCustomerSerializer(user_customer, data=request.data)
-			if serializer.is_valid():
-				serializer.save()
-				return Response(serializer.data)
-			return Response({"message":"Данные не корректный!"}, status=400)
-
-		elif request.method == 'DELETE':
-			user_customer.delete()
-			return Response({"message":"Данные удалены!"}, status=204)
-	except UserCustomer.DoesNotExist:
-		return Response({"message":"Данные не найдены!"}, status=404)
-
-
-@api_view(['GET', 'PUT'])
-def user_detail(request, pk):
-	try:
-		user = User.objects.get(pk=pk)
-		if request.method == 'GET':
-			serializer = UserDetailsSerializer(user)
-			return Response(serializer.data)
-		elif request.method == 'PUT': 
-			serializer = UserUpdateSerializer(user, data=request.data)
-			if serializer.is_valid():
-				serializer.save()
-				return Response( UserDetailsSerializer(user).data )
-			return Response({"message":"Данные не корректный!"}, status=400)
-
-	except User.DoesNotExist:
-		return Response({"message":"Данные не найдены!"}, status=404)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserDetailsSerializer
