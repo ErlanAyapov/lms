e11db76943612e2a86f2149097c2acc65352692c
ErlanAyapov <simsiti0009ggg@gmail.com>
2023-05-26 13:06:45 +0600

    added new conf

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost".split(",") )
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1", "localhost" )
 
 
 CORS_ORIGIN_WHITELIST = [
